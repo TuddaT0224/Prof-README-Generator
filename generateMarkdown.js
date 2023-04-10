70fdb080300e5b118d98eda478bae8de2c271bd9
Tudda <trezzt0224@gmail.com>
2023-04-10 14:57:04 -0500

    Updated index.js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,20 +1,35 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  // return a badge
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== 'NONE') {
+    return '*[License](#-Contributing)';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if(license !== 'NONE') {
+    return '# License';
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
   # Project Title
    ${data.title}
+   ${renderLicenseBadge(data.getLicense)}
    ${data.getLicense}
 
   # Description
@@ -23,10 +38,10 @@ function generateMarkdown(data) {
   # Table of Contents
    *[Installation](#-Installation)
    *[Usage](#-Usage)
-   *[License](#-Contributing)
    *[Contributing](#-Contributing)
    *[Tests](#-Tests)
    *[Questions](#-Contact-Information)
+   ${renderLicenseLink(data.getLicense)}
 
   # Installation
    ${data.installation}
@@ -34,7 +49,8 @@ function generateMarkdown(data) {
   # Usage
    ${data.usage}
 
-  # License
+  ${renderLicenseSection(data.getLicense)}
+  ${renderLicenseBadge(data.getLicense)}
    ${data.license}
    * As this license list was not comprehensive, if you need another license, use the contact information below to ask for license to be added.
 
--- index.js
+++ index.js
@@ -1,13 +1,73 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // Questions for the title
+    {
+       type: "input",
+       name: "title",
+       message: "What is the title of your project?"
+    },
+    // Question for the project Description
+    {
+        type: "input",
+        name: "description",
+        message:"Please enter a description of your project.",
+    },
+    // Questions for Installation
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter a explanation of how to install the software",
+    },
+    // Question for Usage
+    {
+        type: "input",
+        name: "usage",
+        message: "Describe how we would use this project",
+    },
+    // Question for License
+    {
+        type: "list",
+        name: "getLicense",
+        message: "Choose a license",
+        choices: ["MIT", "NONE", "Apache"],
+    },
+    // Questions for Contributing
+    {
+        type: "input",
+        name: "contributing",
+        message: " How users would contribut to your project",
+    },
+    // Questions for GitHub username
+    {
+        type: "input",
+        name: "userName",
+        message: "What is your GitHub userName?",
+    },
+    // Questions for Email Address
+    {
+        type: "input",
+        name: "userEmail",
+        message: "What is your Email Address?",
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+      err ? console.log(err) : console.log('Successfully created index.html!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    // inquirer prompt
+    // then writeToFile 
+}
 
 // Function call to initialize app
 init();
